57a334581ae6be56aa65eddcd17ed58e50e3488b
Nikita <nikitaloginovxxx1986@gmail.com>
2024-10-07 22:56:48 +0300

    final

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -8,10 +8,13 @@ export const List = ({ notes, onRemove, onCheked, onEdit, isEditInput }) => {
                 <li className={ListStyle.liContainer} key={note.id}>
                     <div className={ListStyle.containerFlex}>
                         <div>
-                            <input type="checkbox" checked={note.isChecked} onChange={() => onCheked(note.id)} />
-                            {note.isEdit ? 
-                            <input className={note.isChecked ? ListStyle.spanText : ListStyle.spanTextNone} onChange={e => isEditInput(note.id, e)} value={note.title} /> 
-                            : <span  className={note.isChecked ? ListStyle.spanText : ListStyle.spanTextNone}>{note.title}</span>}
+                            <label className={ListStyle.customСheckbox}>
+                                <input type="checkbox" checked={note.isChecked} onChange={() => onCheked(note.id)} />
+                                <span className={ListStyle.checkmark}></span>
+                            </label>
+                            {note.isEdit ?
+                                <input className={note.isChecked ? ListStyle.spanText : ListStyle.spanTextNone} onChange={e => isEditInput(note.id, e)} value={note.title} />
+                                : <span className={note.isChecked ? ListStyle.spanText : ListStyle.spanTextNone}>{note.title}</span>}
                         </div>
                         <div className="">
                             <button className={ListStyle.btnEdit} onClick={() => onEdit(note.id)}><img className={ListStyle.edit} src="./images/Frame 6.svg" alt="" /></button>
